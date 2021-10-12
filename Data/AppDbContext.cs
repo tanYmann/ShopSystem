01fbdf9b4a103c4915abc6cd3b2a48e78d988129
tanYmann <75977773+tanYmann@users.noreply.github.com>
2021-10-12 11:15:35 +0200

    Add files via upload

---
+++ Data/AppDbContext.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.EntityFrameworkCore;
+using ShopSystem.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace ShopSystem.Data
+{
+    public class AppDbContext : DbContext
+    {
+        public AppDbContext(DbContextOptions<AppDbContext> options):base(options)
+        {
+
+        }
+        DbSet<Address> Addresses { get; set; }
+        DbSet<Customer> Customers { get; set; }
+        DbSet<DeliveryNote> DeliveryNotes { get; set; }
+        DbSet<Invoice> Invoices { get; set; }
+        DbSet<Item> Items { get; set; }
+        DbSet<Order> Orders { get; set; }
+        DbSet<Position> Positions { get; set; }
+
+    }
+}
